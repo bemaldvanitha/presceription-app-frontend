f8ee017f0b0671fe96d82a9d23c25b8e53ebe1f1
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-07-15 05:31:35 +0530

    added token expire logic

--- src/slicers/apiSlice.js
+++ src/slicers/apiSlice.js
@@ -6,8 +6,20 @@ const baseQuery = fetchBaseQuery({
     baseUrl: BASE_URL
 });
 
+const baseQueryWithReAuth = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+    if(result?.error && result?.error?.status === 401){
+        localStorage.removeItem('token');
+        window.location.href = '/login';
+    }
+    if(result?.error && result?.error?.status === 403){
+        window.history.back();
+    }
+    return result;
+}
+
 export const apiSlice = createApi({
-    baseQuery: baseQuery,
+    baseQuery: baseQueryWithReAuth,
     tagTypes: [],
     endpoints: (builder) => ({
 
